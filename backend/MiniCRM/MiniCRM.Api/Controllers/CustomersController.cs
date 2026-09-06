21d199eab77dfb597b8a0b68faa01aef60212285
Sal Allegra <salallegra@gmail.com>
2026-09-06 12:23:13 -0700

    Initial create, create Api skeleton

---
+++ backend/MiniCRM/MiniCRM.Api/Controllers/CustomersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniCRM.Api.Models;
+
+namespace MiniCRM.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomersController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<Customer> GetCustomer()
+    {
+        var customer = new Customer
+        {
+            Id = 1,
+            Name = "John Smith",
+            Email = "john@example.com"
+        };
+
+        return Ok(customer);
+    }
+}
